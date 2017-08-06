33dfa882b0a9c0659bc35d43876ce856e9c48fcb
leandro19br <leandro19br@gmail.com>
2017-08-06 13:10:45 -0300

    commit inicial

---
+++ src/view/Main.java
@@ -0,0 +1,15 @@
+package view;
+
+import controller.SoController;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		SoController so = new SoController();
+
+		System.out.println(so.os());
+
+	}
+
+}
